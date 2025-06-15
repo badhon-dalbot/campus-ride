e27dac61fe74c0bdf6b572720bcec09090cf106e
Sayem Reza <mreza223363@bscse.uiu.ac.bd>
2025-06-16 03:24:58 +0600

    added payment chatting

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,29 +1,38 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home.jsx';
-import Login from '../pages/Login.jsx';
+import Login from '../pages/LogIn.jsx';
 import SignUp from '../pages/SignUp.jsx';
+import Home from '../pages/Home.jsx'
 import FindRide from '../pages/FindRide.jsx'
 import ShareRide from '../pages/ShareRide.jsx'
 import BookRide from '../pages/BookRide.jsx'
 import RideDetails from '../pages/RideDetails.jsx';
 import ConfirmBooking from '../pages/ConfirmBooking.jsx';
 import DriverProfile from '../pages/DriverProfile.jsx';
-import AboutUs from '../pages/AboutUs.jsx';
+import Payment from '../pages/Payment.jsx';
+import Chatting from '../pages/Chatting.jsx';
 
+
+
+// Comment out Login and Root unless you have them created
+// import Login from '../pages/Login';
+// import Root from '../pages/Root';
 
 const AppRoutes = () => (
   <Routes>
     {/* Temporary home page to test routing */}
-    <Route path="/" element={<Home/>} />
+    <Route path="/" element={<h1>Home Page</h1>} />
     <Route path="/login" element={<Login />} />
     <Route path="/signup" element={<SignUp />} />
+    <Route path="/Home" element={<Home/>}/>
     <Route path="/findride" element={<FindRide/>}/>
     <Route path="/shareride" element={<ShareRide/>}/>
     <Route path="/bookride" element={<BookRide/>}/>
     <Route path="/ride-details" element={<RideDetails/>}/>
     <Route path="/conf-book" element={<ConfirmBooking/>}/>
     <Route path="/driverProfile" element={<DriverProfile/>}/>
-    <Route path="/aboutus" element={<AboutUs/>}/>
+    <Route path="/payment" element={<Payment/>}/>
+    <Route path="/chatting" element={<Chatting/>}/>
+    
     
 
     {/* Comment out or remove these unless you define them */}
