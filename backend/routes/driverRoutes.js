264f8a063d72698a678fad42cbac6e446cfbe5b6
badhon-dalbot <bdalbot116@gmail.com>
2025-06-26 23:24:23 +0600

    driver dashboard api

--- backend/routes/driverRoutes.js
+++ backend/routes/driverRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
+import {
+  getDriverDashboard,
+  getDriverProfile,
+  updateDriverBio,
+  updatePreferences,
+} from "../controllers/driverController.js";
 import { getDriverRides } from "../controllers/rideController.js";
-import { getDriverProfile, updatePreferences, updateDriverBio } from '../controllers/driverController.js';
 const router = express.Router();
 
-router.get('/:id/profile', getDriverProfile);
-router.get('/:id/active-rides', getDriverRides)
-router.put('/:id/preferences', updatePreferences);
-router.put('/:id/bio', updateDriverBio);
-
+router.get("/:id/profile", getDriverProfile);
+router.get("/:id/active-rides", getDriverRides);
+router.put("/:id/preferences", updatePreferences);
+router.put("/:id/bio", updateDriverBio);
+router.get("/:id/dashboard", getDriverDashboard);
 
 export default router;
